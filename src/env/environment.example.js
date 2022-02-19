10a0dec699e9a862448d14a791e15ea69f4531ae
Felipe Wesley <fe.wesleybasso@gmail.com>
2022-02-18 23:02:52 -0300

    example environment file

---
+++ src/env/environment.example.js
@@ -0,0 +1,21 @@
+/**
+ * Environment variables - Example File
+ * 
+ * Este arquivo deve ser substituido por um arquivo de environment local.
+ * Para fazer a substituição, duplique este arquivo e remova a extensão '.example', mantendo apenas '.js'
+ * 
+ * - O novo arquivo deve conter o nome 'environment.js'
+ * 
+ * O novo arquivo criado será ignorado pelo git changes, 
+ * evitando que suas credenciais locais de desenvolvimento sejam publicadas,
+ * considerando que será utilizado de acordo com o ambiente local de desenvolvimento
+ */
+
+module.exports = {
+
+  /** Application name */
+  applicationName: 'Node SMTP Mail',
+
+  /** Server port */
+  port: 3000,
+};
