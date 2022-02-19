5b4b1d174426e96c05142db8d3fbfc9857b65cca
Felipe Wesley <fe.wesleybasso@gmail.com>
2022-02-19 00:08:35 -0300

    disable cors module temporarily

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 // Set the request body using json data type
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 module.exports = app;
