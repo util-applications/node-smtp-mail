8e6af752c077a767b708fec8c62f1b70e6c5dcdf
Felipe Wesley <fe.wesleybasso@gmail.com>
2022-02-21 23:21:02 -0300

    email templates enumeration

---
+++ src/templates/templates.js
@@ -0,0 +1,10 @@
+/**
+ * Enumeration for local templates - Found more easily by this enumeration
+ */
+const templates = {
+
+  helloWorld: "/main/hello-world.html",
+
+}
+
+module.exports = templates;
