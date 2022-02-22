b49cab90f03fc8beefc4d281b13d3057a92f76f4
Felipe Wesley <fe.wesleybasso@gmail.com>
2022-02-21 22:26:52 -0300

    application startup class - start routings method

---
+++ server/startup.js
@@ -0,0 +1,24 @@
+const NodeSmtpMailStartup = require("../src/core/startup/node-smtp-mail-startup");
+/**
+ * @class Startup
+ * 
+ * Registro de rotas disponibilizadas por esta API através do método `start`
+ * 
+ * @method super.addRouting Adiciona ao core da aplicação um entry-point a uma rota customizada - Deve ser chamado em start
+ */
+class Startup extends NodeSmtpMailStartup {
+
+  constructor() { }
+
+  /**
+   * Método onde é possível registrar os entry points da aplicação com suas rotas customizadas
+   * @method start
+   */
+  start() {
+
+    this.addRouting("/", require("../src/api/routes/main-routes"));
+
+  }
+}
+
+module.exports = new Startup();
