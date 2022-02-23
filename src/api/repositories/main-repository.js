145e3aeb004fda9c8ed35713b01749ad707871bf
Felipe Wesley <fe.wesleybasso@gmail.com>
2022-02-22 23:16:37 -0300

    criação main repository - exemplificar utilização de repositories

---
+++ src/api/repositories/main-repository.js
@@ -0,0 +1,28 @@
+const env = require("../../env/environment");
+
+class MainRepository {
+
+  constructor() { }
+
+  /**
+   * Obter destinatario para o envio de e-mail
+   * @param {import('express').Request} req 
+   * @returns {Promise<string>}
+   */
+  async obterDestinatario(req) {
+
+    return req.body['email'] ?? env.email.smtp.account.username;
+  }
+
+  /**
+   * Obter mensagem de response inicial
+   * @returns {Promise<string>}
+   */
+  async obterResponseMessage() {
+
+    return "Hello world";
+  }
+  
+}
+
+module.exports = MainRepository;
