ee25984b824abb5aa3a5bcae5619f33a3289d9bd
Felipe Wesley <fe.wesleybasso@gmail.com>
2022-03-05 13:08:59 -0300

    chamada template builder - header

---
+++ pages/global/js/script.js
@@ -0,0 +1,8 @@
+import { buildMainHeader } from './template-builder.js';
+
+const header = document.getElementById('main-header');
+const footer = document.getElementById('main-footer');
+
+buildMainHeader(header);
+
+// console.log(a);
