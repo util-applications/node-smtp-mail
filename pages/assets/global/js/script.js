f1e6b735a86e1375448b0c60efd205c338eb5e16
Felipe Wesley <fe.wesleybasso@gmail.com>
2022-03-05 17:30:38 -0300

    setup inicial de header - main script

--- pages/assets/global/js/script.js
+++ pages/assets/global/js/script.js
@@ -1,8 +1,15 @@
 import { buildMainHeader } from './template-builder.js';
+import { PLATFORM_NAME } from '../../data/platform-name.js';
 
 const header = document.getElementById('main-header');
-const footer = document.getElementById('main-footer');
 
+if (!header)
+  throw new Error("Elemento <header> não encontrado. Crie um header com o id 'main-header'");
+
+// Construindo estrutura header principal da aplicação
 buildMainHeader(header);
 
-// console.log(a);
+// Preenchendo nome do projeto no template
+for (let ref of document.getElementsByClassName("platform-name")) {
+  ref.innerHTML = PLATFORM_NAME;
+}
