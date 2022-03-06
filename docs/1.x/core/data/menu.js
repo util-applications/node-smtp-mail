1a4290fa2c1cfa31a10b0b45d9c0528675b69409
Felipe Wesley <fe.wesleybasso@gmail.com>
2022-03-06 01:14:16 -0300

    rebuild base href

--- docs/1.x/core/data/menu.js
+++ docs/1.x/core/data/menu.js
@@ -5,17 +5,17 @@
 export const menu = [
   {
     label: "Início",
-    link: "/1.x/index.html",
+    link: "/node-smtp-mail/docs/1.x/index.html",
     available: true
   },
   {
     label: "Getting Started",
-    link: "/1.x/getting-started.html",
+    link: "/node-smtp-mail/docs/1.x/getting-started.html",
     available: true
   },
   {
     label: "Environment",
-    link: "/1.x/environment.html",
+    link: "/node-smtp-mail/docs/1.x/environment.html",
     available: true
   }
 ];
